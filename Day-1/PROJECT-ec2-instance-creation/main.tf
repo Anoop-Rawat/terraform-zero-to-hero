a1cb0a46dee9af97bf2191ec78ef03fe21e976f5
Anoop Singh Rawat <112460114+Anoop-Rawat@users.noreply.github.com>
2025-05-15 12:49:52 +0530

    Update main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0f88e80871fd81e91"  # Specify an appropriate AMI ID
+    ami           = "ami-053b0d53c279acc90"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
 }
 
