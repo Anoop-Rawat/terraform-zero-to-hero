793656a3c934c1eca050734901e3bd92d2d97b18
Anoop Singh Rawat <112460114+Anoop-Rawat@users.noreply.github.com>
2025-05-25 20:22:27 +0530

    Delete backend.tf

--- Day-4/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "anoop-s3-demo-xyz-demo" # bucket name
-    key            = "backend/terraform.tfstate" # path with name of file for statefile inside bucket
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "terraform-lock"
-  }
-}
