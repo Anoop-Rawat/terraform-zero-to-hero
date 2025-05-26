2fddcabecd28508e683ab80f4a77ca36e0639cbc
Anoop Singh Rawat <112460114+Anoop-Rawat@users.noreply.github.com>
2025-05-26 16:21:43 +0530

    Update main.tf

--- Day-7 Vault/main.tf
+++ Day-7 Vault/main.tf
@@ -1,24 +1,26 @@
+// youtube link https://www.youtube.com/watch?v=sIkRK33AY50&list=PLdpzxOOAlwvI0O4PeKVV1-yJoX2AqIWuf&index=7
+
 provider "aws" {
   region = "us-east-1"
 }
 
 provider "vault" {
-  address = "<>:8200"
+  address = "http://172.21.184.9:8200"
   skip_child_token = true
 
   auth_login {
     path = "auth/approle/login"
 
     parameters = {
-      role_id = "<>"
-      secret_id = "<>"
+      role_id = "74ca799d-2d83-d3f5-5f1c-64a124027b8c"
+      secret_id = " 8951e2aa-9777-653d-81db-4f2f8668ce40"
     }
   }
 }
 
 data "vault_kv_secret_v2" "example" {
-  mount = "secret" // change it according to your mount
-  name  = "test-secret" // change it according to your secret
+  mount = "kv" // change it according to your mount
+  name  = "MyKVsecret" // change it according to your secret
 }
 
 resource "aws_instance" "my_instance" {
@@ -27,6 +29,6 @@ resource "aws_instance" "my_instance" {
 
   tags = {
     Name = "test"
-    Secret = data.vault_kv_secret_v2.example.data["foo"]
+    Secret = data.vault_kv_secret_v2.example.data["username"]
   }
 }
