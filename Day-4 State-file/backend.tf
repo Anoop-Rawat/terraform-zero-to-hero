16089b9cee49e3382133ede950cb8b3c90f63428
Anoop Singh Rawat <112460114+Anoop-Rawat@users.noreply.github.com>
2025-05-27 19:25:21 +0530

    Update backend.tf

--- Day-4 State-file/backend.tf
+++ Day-4 State-file/backend.tf
@@ -4,6 +4,6 @@ terraform {
     key            = "backend/terraform.tfstate" # path with name of file for statefile inside bucket
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "terraform-lock"
+    dynamodb_table = "terraform-lock" # or use s3 native by using use_lockfile = true
   }
 }
